b4cb7a29aed8a5449b6e4d0a4c5da2af522fc80f
Thiago <>
2021-03-24 02:28:40 +0100

    remove tutorial content

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('articles', 'ArticleController@index');
-Route::get('articles/{article}', 'ArticleController@show');
-Route::post('articles', 'ArticleController@store');
-Route::put('articles/{article}', 'ArticleController@update');
-Route::delete('articles/{article}', 'ArticleController@delete');
-
 Route::get('packages', 'PackagesController@index');
 Route::get('packages/{id}', 'PackagesController@show');
 Route::post('packages', 'PackagesController@store');
