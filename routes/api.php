4ec09c9ac3416d063ec18dc6e66afaadc96849aa
Thiago <>
2021-03-24 02:12:05 +0100

    project commit - tutorial models and migrations etc

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@delete');
