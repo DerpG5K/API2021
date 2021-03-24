7d04c6f793299d16f8695d4d8c7ebb762e1691c1
DerpG5K <48905732+DerpG5K@users.noreply.github.com>
2021-03-24 20:51:48 +0100

    Add files via upload

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PackagesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+        // \App\Models\User::factory(10)->create();
+        $this->call(EmployeesTableSeeder::class);
     }
 }
