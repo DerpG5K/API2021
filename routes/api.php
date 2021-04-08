ff30beaa4c1e11d5171f7b8650575ae0569d75db
asmail baouider <asmailbaouider@gmail.com>
2021-04-08 16:29:40 +0200

    AB added routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Resource controllers
+Route::resource('flights', 'FlightController', ['except' => ['create', 'edit']]);
+Route::resource('parcels', 'ParcelController', ['except' => ['create', 'edit']]);
+Route::resource('addresses', 'AddressController', ['except' => ['create', 'edit']]);
+Route::resource('customs', 'CustomsController', ['except' => ['create', 'edit']]);
+Route::resource('delivery_types', 'DeliveryTypeController', ['except' => ['create', 'edit']]);
+Route::resource('orders', 'OrderController', ['except' => ['create', 'edit']]);
+
+
+Route::resource('orders.parcels', 'ParcelController');
+Route::resource('orders.parcels.address', 'AddressController');
+Route::resource('orders.parcels.customs', 'CustomsController');
+Route::resource('orders.parcels.parcelCheck', 'ParcelCheckController');
+Route::resource('orders.parcels.flight', 'FlightController');
+Route::resource('orders.parcels.deliveryType', 'DeliveryTypeController');
+Route::resource('orders.parcels.parcelType', 'ParcelTypeController');
+Route::resource('orders.customer', 'CustomerController');
