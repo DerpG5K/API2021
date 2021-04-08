d11f81fb42dd658018fe6003f8acc8127f38faf0
DerpG5K <48905732+DerpG5K@users.noreply.github.com>
2021-04-08 12:23:33 +0200

    Add files via upload

---
+++ app/Http/Controllers/CustomsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Custom;
+use Illuminate\Http\Request;
+
+class CustomsController extends Controller
+{
+    public function index()
+    {
+        return Custom::all();
+    }
+
+    public function show(Custom $custom)
+    {
+        return $custom;
+    }
+
+    public function store(Request $request)
+    {
+        $custom = Custom::create($request->all());
+
+        return response()->json($custom, 201);
+    }
+
+    public function update(Request $request, Custom $custom)
+    {
+        $custom->update($request->all());
+
+        return response()->json($custom, 200);
+    }
+
+    public function delete(Custom $custom)
+    {
+        $custom->delete();
+
+        return response()->json(null,204);
+    }
+}
