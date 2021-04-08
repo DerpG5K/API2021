53e9241187b34164bdc3f560f3ffff32540a8021
asmail baouider <asmailbaouider@gmail.com>
2021-04-08 16:31:00 +0200

    AB added routes parcelchecks and types

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,8 @@ Route::resource('addresses', 'AddressController', ['except' => ['create', 'edit'
 Route::resource('customs', 'CustomsController', ['except' => ['create', 'edit']]);
 Route::resource('delivery_types', 'DeliveryTypeController', ['except' => ['create', 'edit']]);
 Route::resource('orders', 'OrderController', ['except' => ['create', 'edit']]);
-
+Route::resource('parcel_checks', 'ParcelCheckController', ['except' => ['create', 'edit']]);
+Route::resource('parcel_types', 'ParcelTypeController', ['except' => ['create', 'edit']]);
 
 Route::resource('orders.parcels', 'ParcelController');
 Route::resource('orders.parcels.address', 'AddressController');
