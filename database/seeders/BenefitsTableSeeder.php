befb3c21de8f99051a4c71bda880c2611bf03d48
DerpG5K <48905732+DerpG5K@users.noreply.github.com>
2021-04-11 15:21:45 +0200

    Add files via upload

--- database/seeders/BenefitsTableSeeder.php
+++ database/seeders/BenefitsTableSeeder.php
@@ -17,16 +17,16 @@ class BenefitsTableSeeder extends Seeder
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Benefit::truncate();
-
+            
         $faker = \Faker\Factory::create();
 
         for ($I1 = 0; $I1 < 100; $I1++){
             Benefit::create([
-                'description' => $faker->text($maxNbChars = 20)
+                'description' => $faker->text($maxNbChars = 20) 
             ]);
         }
 
-
+        
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +14,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //$this->call(UserSeeder::class);
+        $this->call(AddressTableSeeder::class);
+        $this->call(DeliveryTypeTableSeeder::class);
+        $this->call(OrderTableSeeder::class);
+        $this->call(ParcelTypeTableSeeder::class);
+        $this->call(CustomTableSeeder::class);
+        $this->call(FlightTableSeeder::class);
+        $this->call(ParcelTableSeeder::class);
+        $this->call(ParcelCheckTableSeeder::class);
+        $this->call(BusinessTableSeeder::class);
+        $this->call(CustomerTableSeeder::class);
+        $this->call(TicketCategoryTableSeeder::class);
+        $this->call(TicketStateTableSeeder::class);
+        $this->call(TicketTablesSeeder::class);
+        $this->call(TicketFileTableSeeder::class);
+        $this->call(TicketLogTableSeeder::class);
+        $this->call(PasswordTablesSeeder::class);
+        $this->call(EmployeeTableSeeder::class);
+        $this->call(AbsencesTableSeeder::class);
+        $this->call(BenefitsTableSeeder::class);
+        $this->call(JobTableSeeder::class);
+        $this->call(JobBenefitsTableSeeder::class);
+        $this->call(JobDescriptionTableSeeder::class);
+        $this->call(JobOfferTableSeeder::class);
+        $this->call(DepartmentTableSeeder::class);
     }
 }
