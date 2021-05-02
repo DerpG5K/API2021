893cee7343b489db6ef6fc30a1fd02150f47ade1
asmail baouider <asmailbaouider@gmail.com>
2021-05-02 02:07:11 +0200

    parcelTypeId added

--- database/seeders/ParcelTableSeeder.php
+++ database/seeders/ParcelTableSeeder.php
@@ -9,6 +9,7 @@ use App\Models\Flight;
 use App\Models\Order;
 use App\Models\Parcel;
 use App\Models\ParcelTpe;
+use App\Models\ParcelType;
 use App\Models\Shipment;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -28,13 +29,14 @@ class ParcelTableSeeder extends Seeder
         $faker = \Faker\Factory::create();
         $shipments = Shipment::all()->pluck('id')->toArray();
         $flights = Flight::all()->pluck('id')->toArray();
-
+        $parcelTypes = ParcelType::all()->pluck('id')->toArray();
         // And now, let's create a few articles in our database:
         for ($i = 0; $i < 500; $i++) {
 
 
             Parcel::create([
                 'shipmentId' => $faker->randomElement($shipments),
+                'parcelTypeId' => $faker->randomElement($parcelTypes),
                 'trackingNumber' => 'ABC'.$faker->randomNumber(9, true).$i,
                 'insurance' => $faker->boolean(50),
                 'weight' => $faker->numberBetween(1, 100),
